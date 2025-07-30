ee8e1d3fdfb32c996c3def5fe635492e2846553f
airmarsin <veme1900@gmail.com>
2025-07-30 13:06:55 +0100

    Your message about the changes

--- http.js
+++ http.js
@@ -1,18 +1,19 @@
-const http = require('http');
+const http = require("http");
 
-const name = 'victor'
+const name = "clara";
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('welcome to our home page ' + name)
-    } else if (req.url === '/about') {
-        res.end('here is our short history ' + name)
-    } else {
-        res.end(
-            `<h1>opps! ${name}<h1>
+  if (req.url === "/") {
+    res.end("welcome to our home page " + name);
+  } else if (req.url === "/about") {
+    res.end("here is our short history " + name);
+  } else {
+    res.end(
+      `<h1>opps! ${name}<h1>
             <p>we can not find the page you are looking for</p>
-            <a href="/">back home</a>`)
-    }
-})
+            <a href="/">back home</a>`
+    );
+  }
+});
 
-server.listen(9000)
+server.listen(9000);
