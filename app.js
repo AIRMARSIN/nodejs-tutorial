70a33d8d087a20e3bb5b8b36c2422d944f497ebb
airmarsin <veme1900@gmail.com>
2025-07-30 12:15:36 +0100

    commit: Add lodash flattenDeep example

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-const http = require('http');
+const _ = require('lodash');
 
-const name = 'victor'
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items);
+console.log(items);
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('welcome to our home page ' + name)
-    } else if (req.url === '/about') {
-        res.end('here is our short history ' + name)
-    } else {
-        res.end(
-            `<h1>opps! ${name}<h1>
-            <p>we can not find the page you are looking for</p>
-            <a href="/">back home</a>`)
-    }
-})
-
-server.listen(9000)
+console.log(newItems); 
